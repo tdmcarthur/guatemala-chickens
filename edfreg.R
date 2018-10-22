92b6ff469b7271faa437b035af4f21e6a3cbbd44
tdmcarthur <travis.d.mcarthur@gmail.com>
2018-10-21 20:17:27 -0400

    Created edfreg.R

---
+++ edfreg.R
@@ -0,0 +1,89 @@
+
+
+
+# WARNING: Say dont allow any missing values, since have not check that yet
+
+edfreg <- function(model, linfct) {
+  # So the function should take as arguments:
+  # felm model
+  # glht()-compatible string or string vector for the hypothesis test that will form w and w_0
+  
+  # check the arguments:
+
+  
+  stopifnot(class(model) == "felm")
+  # Make sure model is felm
+  stopifnot("cX" %in% names(model))
+  # (check that "cX" %in% names(model))
+  stopifnot(length(model$clustervar) == 1)
+  # Allow one and only one cluster variable for now
+  
+  x <- model$cX
+  # take the cX matrix
+  
+  # Check that ... well the glht() fujction itself will check the linfct input
+  
+  
+  # form w and w_0 form the string vector and glht()
+  
+  glht.setup <- glht(model, linfct = linfct)
+  
+  w <- glht.setup$linfct # hypothesis matrix
+  w_0 <- glht.setup$rhs # RHS of hypothesis test vector
+  
+  # do the stuffs
+  
+  x <- model$cX
+  beta <- coef(model)
+  e <- resid(model)
+  e.var.est <- as.vector(crossprod(e) / model$df.residual)
+  # Remove 1 x 1 matrix dimensions with as.vector() so
+  # don't have non-conformable matrix down th eline
+  
+  x.cross.inv <- solve(crossprod(x))
+  x.times.x.cross.inv <- x %*% x.cross.inv
+  
+  z <- x.times.x.cross.inv %*% w
+  
+  clusters <- model$clustervar[[1]]
+  # clusters <- factor(rep(1:3, each = 4))
+  num.clusters <- length(unique(clusters))
+  clust.mat <- model.matrix( ~ -1 + clusters)
+  
+  z <- as.vector(z)
+  # strips away dimensions
+  block_z <- (z * clust.mat) %*% t(z * clust.mat)
+  # rep(5:8, each = 3) * clust.mat
+  # Note: non-conformable matrices. z vector (which is N x 1) duplicates itself
+  # into a N x K matrix first, then element-by-element multiplication with clust.mat
+  
+  M <- diag(nrow = N) - x.times.x.cross.inv %*% t(x)
+  
+  c <- num.clusters / (num.clusters - 1) *
+    (N - 1) / (N - K)
+  
+  Bx <- c * M %*% block_z %*% M / crossprod(z)
+  
+  Mu <- sum(diag(Bx))
+  
+  v <- 2 * sum(diag(Bx %*% Bx))
+  
+  edf <- 2 * Mu * Mu / v
+  
+  numerator <- (w %*% beta - w_0) /
+    sqrt(e.var.est * w %*% x.cross.inv %*% w)
+  
+  denominator <- sqrt((1 / (2 * Mu ^ 2 / v)) *
+                        (e / sqrt(e.var.est)) %*% (Bx * (2 * Mu / v))  %*% (e / sqrt(e.var.est)))
+  
+  test.statistic <- numerator / denominator
+  
+  edf <- 2 * Mu ^ 2 / v
+  
+  p.value <- 2 * pt((-1) * abs(test.statistic), df = edf)
+  
+  p.value
+  # return a p value
+}
+
+
